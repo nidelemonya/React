68b88b182197af4b2fea897812f8f08b2913b95d
Bruce-shy <zhouzihan341308@163.com>
2020-06-08 21:29:07 +0800

    排行榜页面 api模块化

---
+++ mango-music-chapter/src/api/jsonp.js
@@ -0,0 +1,38 @@
+// 使命是提供对跨域请求的抽象封装
+// 服务于 ranking
+// 服务于 Recommend.js 
+
+// url 不一样
+// params
+
+// es5 模块化
+import originJsonp from "jsonp";
+
+let jsonp = (url, data, option) => {
+    return new Promise((resolve, reject) => {
+        originJsonp(buildUrl(url, data), option, (err, data) => {
+            if  (!err) {
+                resolve(data);
+            }
+            else {
+                reject(err);
+            }
+        })
+    });
+}
+
+function buildUrl(url, data) {
+    let params = [];
+    for (var k in data) {
+        params.push(`${k}=${data[k]}`);
+    }
+    let param = params.join("&");
+    if (url.indexOf("?") === -1) {
+        url += "?" + param;
+    } else {
+        url += "&" + param;
+    }
+    return url;
+}
+
+export default jsonp;
--- mango-music-chapter/src/components/ranking/Ranking.jsx
+++ mango-music-chapter/src/components/ranking/Ranking.jsx
@@ -1,10 +1,69 @@
 import React from 'react';
-import './ranking.styl'; 
+import './ranking.styl';
+import LazyLoad from 'react-lazyload'
+
+// 排行榜 路由级别页面
+// import Loading from '../../common/loading/Loading';
+// alias 路径短链接 相对路径 路径的别名 @
+import Loading from '@/common/loading/Loading';
+import { getRankingList, getRankingInfo } from '@/api/ranking'; // 把请求数据的业务， 分离到api目录下
 
 class Ranking extends React.Component {
-    render(){
+    constructor() {
+        super()
+        this.state = {
+            loading: true,
+            rankingList: []
+        }
+    }
+    componentDidMount() {
+        getRankingList()
+            .then((res) => {
+                // console.log(res);
+                console.log('获取排行榜数据', res.data);
+                this.setState({
+                    loading: false,
+                    rankingList: res.data.topList
+                })
+            })
+    }
+    render() {
         return (
-            <div>Ranking</div>
+            <div className="music-ranking">
+                <div className="ranking-list">
+                    {
+                        this.state.rankingList.map(ranking => {
+                            return (
+                                <div className="ranking-wrapper" key={ranking.id}>
+                                    <div className="left">
+                                        <LazyLoad height={100}>
+                                            <img src={ranking.picUrl} alt={ranking.title} />
+                                        </LazyLoad>
+                                    </div>
+                                    <div className="right">
+                                        <h1 className="ranking-title">
+                                            {ranking.title}
+                                        </h1>
+                                        {
+                                            ranking.songList.map((song, index) => {
+                                                return (
+                                                    <div className="top-song" key={index}>
+                                                        <span className="index">{index + 1}</span>
+                                                        <span>{song.songname}</span>
+                                            &nbsp;-&nbsp;
+                                                        <span className="song">{song.singername}</span>
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <Loading show={this.state.loading} title="正在加载排行榜..." />
+            </div>
         )
     }
 }
