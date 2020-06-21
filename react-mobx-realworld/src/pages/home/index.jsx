8ea10a0f2a8f9be65149a61c4ba520207e0b2ce9
Bruce-shy <zhouzihan341308@163.com>
2020-06-21 10:57:24 +0800

    mobx 学习，以及 antd Pagination 分页器的使用

--- react-mobx-realworld/src/pages/home/index.jsx
+++ react-mobx-realworld/src/pages/home/index.jsx
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
+import { Pagination } from 'antd';
 
 // indect 注入
 // 想要哪个页面的数据就注入谁
 @inject('articleStore')
 @observer
 class Home extends Component {
-    state = {  }
+    componentDidMount(){
+        this.props.articleStore.getArticle('all')
+    }
+    handlePaginationChange = (page) => {
+        // 1 offset 0
+        // 2 offset 1
+        // 3 offset 2
+        this.props.articleStore.getArticle('all', page - 1);
+    }
     render() { 
+        const { total, LIMIT } = this.props.articleStore
         return (
             <div>
-                { this.props.articleStore.all.length}
+                { this.props.articleStore.articles.all.length}
+                <Pagination 
+                onChange={this.handlePaginationChange}
+                total={total} 
+                pageSize={LIMIT}
+                defaultCurrent={1}/>
             </div>
         );
     }
--- react-mobx-realworld/src/store/articleStore.js
+++ react-mobx-realworld/src/store/articleStore.js
@@ -5,23 +5,26 @@ class ArticleStore  {
   // observable state  用 observable 修饰过的变量
   // 只要变量被修改了 页面就会重新渲染
   // es @ 修饰器
+  LIMIT = LIMIT;
   @observable articles = {
     all:[],
     cars:[]
   }
-
+  @observable total = 0;
+  // 逻辑 尽量写到 store
   @action 
   getArticle(tag, offset = 0){
     axios.get('/articles', {
       params:{
-        tag,
+        tag: tag === 'all' ? null : tag,
         offset,
         limit: LIMIT
       }
     })
     .then(res => {
-      // 修改 store
+      // 修改 store 在这一步完成
       this.articles[tag] = res.articles
+      this.total = res.articlesCount
     })
   }
 }
